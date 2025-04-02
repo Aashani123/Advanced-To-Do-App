f71a70e48b16509508367ed1d45d675421969b34
Ashani T ranasingha <105184856+Aashani123@users.noreply.github.com>
2025-04-02 07:08:00 +0530

    Add Font Awesome icons to SingleTodo component and update dependencies

--- src/components/TodoList/SingleTodo.tsx
+++ src/components/TodoList/SingleTodo.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Todo } from '../../model';
 import { AiFillEdit } from 'react-icons/ai';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck, faDeleteLeft, faEdit, faRightLeft, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 type Props = {
     todo: Todo,
@@ -14,7 +16,13 @@ const SingleTodo = ({todo, todos, setTodos}: Props) => {
       <span className='todos_single_text'>{todo.todo}</span>
       <div>
         <span className="icon">
-          <AiFillEdit />
+        <FontAwesomeIcon icon={faEdit} />
+        </span>
+        <span className="icon">
+        <FontAwesomeIcon icon={faTrash} />
+        </span>
+        <span className="icon">
+        <FontAwesomeIcon icon={faCheck} />
         </span>
       </div>
     </form>
